f8e3674dcc059e4e3d8db289aac6bfac1a9a4881
talonthorn <kweinhold@yahoo.com>
2018-10-07 21:27:30 +0000

    Added Update

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({
    extended: true
 }));
 
-var server = app.listen(7777, function() {
+var server = app.listen(7776, function() {
    var port = server.address().port;
 
    console.log("Listening at %s", port);
@@ -50,24 +50,28 @@ app.get("/cats", function(req, res) {
 
 //retreive single record
 app.get("/cats/:id", function(req, res) {
-   connection.query("select * from Cats where Cindex=?", [req.params.id], function (error, results, fields) {
-      if (error) throw error;
-      res.status(201).end(JSON.stringify(results));
-   });
-});
-
-//update a record
-app.put('/cats', function (req, res) {
-   connection. query('update Cats set Breed=?, Country=?, Origin=?, Coat=? where Cindex=?', [req.body.Breed, req.body.Country, req.body.Origin, req.body.Coat, req.body.Id], function (error, results, fields) {
+   var params = req.params;
+   connection.query("select * from Cats where Cindex=?", [params.id], function (error, results, fields) {
       if (error) throw error;
       res.status(200).end(JSON.stringify(results));
    });
 });
 
+//update a record
+app.put('/cats', function (req, res) {
+   var params = req.body;
+   console.log("Update record ", params.id, ": Origin=", params.origin);
+   connection. query('update Cats set Origin=? where Cindex=?', [params.origin, params.id], function (error, results, fields) {
+      if (error) throw error;
+      res.status(201).end(JSON.stringify(results));
+   });
+});
+
 //delete a record
-app.delete('/cats', function (req, res) {
-   console.log('Delete record: ', req.body);
-   connection.query('delete from Cats where Cindex=?', [req.params.Id], function (error, results, fields) {
+app.delete('/cats/:id', function (req, res) {
+   var params = req.params;
+   console.log('Delete record: ', params.id);
+   connection.query('delete from Cats where Cindex=?', [params.id], function (error, results, fields) {
       if (error) throw error;
       res.status(201).end('Record has been deleted.');
    });
